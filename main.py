fea5f9ab9d5def69d1a0bebdc23750554dc3fc64
Antonio Rodriguez <antonioinorge@hotmail.com>
2025-02-16 18:51:24 +0100

    create functions to create csv file from kmz

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
+import zipfile 
+import os 
+import fiona 
+import pandas as pd 
 import geopandas as gpd 
 
+KML_FILE = "data/doc.kml"
 
-gdf = gpd.read_file("EV-friendly hotels in Europe.kml", driver='libkml')
-print(gdf.head())
+def extract_kml():
+    kmz_file_path = "data/EV-friendly hotels in Europe.kmz"  
+    extraction_dir = os.path.dirname("data/EV-friendly hotels in Europe.kmz")
+    fiona.drvsupport.supported_drivers["libkml"] = "rw" 
+    fiona.drvsupport.supported_drivers["LIBKML"] = "rw"
+    with zipfile.ZipFile(kmz_file_path, "r") as kmz:
+        kmz.extractall(extraction_dir)
+
+def get_kml_layers(kml_file):
+    layers_list = []
+    for layer in fiona.listlayers(kml_file) :    
+        df = gpd.read_file(kml_file, driver="LIBKML", layer=layer)
+        layers_list.append(df)
+    return layers_list
+
+def create_csv(func_get_kml_layers):
+    df = gpd.GeoDataFrame(pd.concat(func_get_kml_layers, ignore_index=True))
+    df_columns = [col for col in df.columns]
+    if "geometry" in df_columns:
+        df["geometry"] = df["geometry"].astype(str)
+
+    df[["longitude", "latitude"]] = df["geometry"].str.extract(r'POINT Z?\s*\(([-\d.]+) ([-\d.]+)(?: [-\d.]*)?\)')
+    df[["longitude", "latitude"]] = df[["longitude", "latitude"]].astype(float)
+    df["Maps link"] = df.apply(lambda row: f"https://www.google.com/maps/place/{row['latitude']},{row['longitude']}", axis=1)
+    df.drop(columns=["geometry"], inplace=True)
+    df.to_csv("data/EV-friendly hotels in Europe.csv", index=False)
+
+
+if __name__ == "__main__":
+    extract_kml()
+    create_csv((get_kml_layers(KML_FILE)))
