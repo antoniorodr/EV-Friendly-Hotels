51ba6cdf9eaa676b49ef0a54810e37bbd260ae4c
Antonio Rodriguez <antonioinorge@hotmail.com>
2025-03-04 11:10:49 +0100

    small changes

--- server.py
+++ server.py
@@ -18,6 +18,8 @@ from io import BytesIO
 
 app = Flask(__name__)
 app.secret_key = os.environ["secret_key"]
+
+
 class Base(DeclarativeBase):
     pass
 
@@ -28,13 +30,21 @@ db.init_app(app)
 Bootstrap5(app)
 csrf = CSRFProtect(app)
 
+
 class SearchForm(FlaskForm):
-    name = StringField("Search for a charge station", validators=[DataRequired(), Length(1, 40)])
+    name = StringField(
+        "Search for a charge station", validators=[DataRequired(), Length(1, 40)]
+    )
     submit = SubmitField("Submit")
 
+
 class TokenForm(FlaskForm):
-    email = StringField("Write your email and click submit to get the token", validators=[DataRequired(), Length(7, 70)])
+    email = StringField(
+        "Write your email and click submit to get the token",
+        validators=[DataRequired(), Length(7, 70)],
+    )
     submit = SubmitField("Submit")
+
 
 class Charger(db.Model):
     __tablename__ = "chargers"
@@ -47,10 +57,13 @@ class Charger(db.Model):
     maps_link: Mapped[str] = mapped_column(String(250), nullable=False)
 
     def to_dict(self):
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
-    
+        return {
+            column.name: getattr(self, column.name) for column in self.__table__.columns
+        }
+
+
 class Token(db.Model):
-    __tablename__= "tokens"
+    __tablename__ = "tokens"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     token: Mapped[str] = mapped_column(String(250), nullable=False)
     email: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
@@ -59,22 +72,24 @@ class Token(db.Model):
 with app.app_context():
     db.create_all()
 
+
 def csv_to_db():
-    with open ("data/EV-friendly chargers in Europe.csv") as file:
+    with open("data/EV-friendly chargers in Europe.csv") as file:
         reader = csv.DictReader(file)
         for row in reader:
             with app.app_context():
                 new_charger = Charger(
-                    name=row["Name"], 
-                    type=row["layer_name"], 
-                    description=row["Description"],  
-                    longitude=row["longitude"],  
-                    latitude=row["latitude"],  
-                    maps_link=row["Maps link"],  
+                    name=row["Name"],
+                    type=row["layer_name"],
+                    description=row["Description"],
+                    longitude=row["longitude"],
+                    latitude=row["latitude"],
+                    maps_link=row["Maps link"],
                 )
 
                 db.session.add(new_charger)
                 db.session.commit()
+
 
 def generate_token():
     token = secrets.token_urlsafe(20)
@@ -83,7 +98,7 @@ def generate_token():
 
 @app.route("/")
 def home():
-  return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/all")
@@ -91,7 +106,7 @@ def all_chargers():
     my_map = folium.Map(location=[52.8806586, 14.559359], zoom_start=4)
     page = request.args.get("page", 1, type=int)
     charger_type = request.args.get("type", None)
-    
+
     charger_types = db.session.query(Charger.type).distinct().all()
     charger_types = [ht[0] for ht in charger_types]
 
@@ -102,26 +117,34 @@ def all_chargers():
     all_relevant_chargers = query.all()
 
     for charger in all_relevant_chargers:
-        folium.Marker(
-            [charger.latitude, charger.longitude],
-            popup=charger.name
-        ).add_to(my_map)
+        folium.Marker([charger.latitude, charger.longitude], popup=charger.name).add_to(
+            my_map
+        )
 
     map_buffer = BytesIO()
     my_map.save(map_buffer, close_file=False)
     map_html = map_buffer.getvalue().decode()
 
-    return render_template("all_chargers.html", chargers=chargers, charger_types=charger_types, map_html=map_html, selected_type=charger_type)
+    return render_template(
+        "all_chargers.html",
+        chargers=chargers,
+        charger_types=charger_types,
+        map_html=map_html,
+        selected_type=charger_type,
+    )
 
-@app.route("/search", methods=['GET', 'POST'])
+
+@app.route("/search", methods=["GET", "POST"])
 def search():
     form = SearchForm()
     message = ""
     chargers = []
-    
+
     if form.validate_on_submit():
         pattern = f"%{form.name.data}%"
-        result = db.session.execute(db.select(Charger).where(Charger.name.ilike(pattern)))
+        result = db.session.execute(
+            db.select(Charger).where(Charger.name.ilike(pattern))
+        )
         chargers = result.scalars().all()
         if chargers:
             if len(chargers) == 1:
@@ -140,68 +163,97 @@ def charger(charger_id):
         return "charger not found", 404
     return render_template("charger.html", charger=charger)
 
-@app.route("/get_token", methods = ["GET", "POST"])
+
+@app.route("/get_token", methods=["GET", "POST"])
 def get_token():
     form = TokenForm()
     if form.validate_on_submit():
         email_check = re.compile(r"^(?!\.)[\w\-_.]+[^.]@\w+([\.-]\w+)*\.\w{2,}$")
         result = re.search(email_check, form.email.data)
-        check_user = db.session.execute(db.select(Token).where(Token.email == form.email.data))
+        check_user = db.session.execute(
+            db.select(Token).where(Token.email == form.email.data)
+        )
         user = check_user.scalar()
         if user:
-            flash(Markup("You have already received a token. Please check your email. If you can't find it or don't remember it, feel free to get in touch through: <a href='mailto:pythoncodingacc69@gmail.com'>pythoncodingacc69@gmail.com</a>"), "info")
+            flash(
+                Markup(
+                    "You have already received a token. Please check your email. If you can't find it or don't remember it, feel free to get in touch through: <a href='mailto:pythoncodingacc69@gmail.com'>pythoncodingacc69@gmail.com</a>"
+                ),
+                "info",
+            )
         if result and not user:
             token = generate_token()
-            new_token = Token(
-                token = token,
-                email = form.email.data
-            )
+            new_token = Token(token=token, email=form.email.data)
             db.session.add(new_token)
             db.session.commit()
             with smtplib.SMTP(os.environ["SMTP_ADDRESS"], port=587) as connection:
                 connection.starttls()
-                connection.login(user=os.environ["email"], password=os.environ["password"])
+                connection.login(
+                    user=os.environ["email"], password=os.environ["password"]
+                )
                 connection.sendmail(
                     from_addr=os.environ["email"],
                     to_addrs=form.email.data,
                     msg=f"Subject:Here is your token!\n\n{token}".encode("utf-8"),
                 )
-                flash("An email with your token has been sent. Check your inbox. If you haven't received an email with the token, please check your junk mail folder.")
+                flash(
+                    "An email with your token has been sent. Check your inbox. If you haven't received an email with the token, please check your junk mail folder."
+                )
     return render_template("token.html", form=form)
 
 
-@app.route("/api/add", methods = ["POST"])
+@app.route("/api/add", methods=["POST"])
 def new_charger():
-    types = ["Hotels and accomodation, charging on private premises", "Charging only (no accomodation)", "Hotel, charging station nearby", "Superchargers", "Tesla Destination Chargers", "Hotel @ Supercharger"]
+    types = [
+        "Hotels and accomodation, charging on private premises",
+        "Charging only (no accomodation)",
+        "Hotel, charging station nearby",
+        "Superchargers",
+        "Tesla Destination Chargers",
+        "Hotel @ Supercharger",
+    ]
     token = request.headers.get("token")
-    check_token_result = db.session.execute(db.select(Token).where(Token.token == token))
+    check_token_result = db.session.execute(
+        db.select(Token).where(Token.token == token)
+    )
     check_token = check_token_result.scalars().first()
     type_ = request.form.get("type")
     if type_ not in types:
-        return jsonify(error={"message": f"The type should be one of the follow: {types}"}), 400
+        return jsonify(
+            error={"message": f"The type should be one of the follow: {types}"}
+        ), 400
     longitude = request.form.get("longitude")
     latitude = request.form.get("latitude")
-    existing_charger = db.session.query(Charger).filter_by(longitude=longitude, latitude=latitude).first()
+    existing_charger = (
+        db.session.query(Charger)
+        .filter_by(longitude=longitude, latitude=latitude)
+        .first()
+    )
     if existing_charger:
-        return jsonify(error={"message": "A charger with this coordinates already exists."}), 400
+        return jsonify(
+            error={"message": "A charger with this coordinates already exists."}
+        ), 400
     if check_token:
         new_charger = Charger(
-            name=request.form.get("name"), 
-            type=request.form.get("layer_name"), 
-            description=request.form.get("description"),  
-            longitude=request.form.get("longitude"),  
-            latitude=request.form.get("latitude"),  
-            maps_link=request.form.get("maps_link"),  
+            name=request.form.get("name"),
+            type=request.form.get("layer_name"),
+            description=request.form.get("description"),
+            longitude=request.form.get("longitude"),
+            latitude=request.form.get("latitude"),
+            maps_link=request.form.get("maps_link"),
         )
         db.session.add(new_charger)
         db.session.commit()
         return jsonify(response={"success": "Successfully added the new charger."}), 200
     return jsonify(error={"error": "You need a valid token."}), 401
 
+
 @app.route("/api/all")
 def all_cafe():
     token = request.headers.get("token")
-    check_token_result = db.session.execute(db.select(Token).where(Token.token == token))
+    check_token_result = db.session.execute(
+        db.select(Token).where(Token.token == token)
+    )
     check_token = check_token_result.scalars().first()
     if check_token:
         result = db.session.execute(db.select(Charger).order_by(Charger.id))
@@ -209,10 +261,13 @@ def all_cafe():
         return jsonify(chargers=[charger.to_dict() for charger in all_chargers])
     return jsonify(error={"error": "You need a valid token."}), 401
 
+
 @app.route("/api/search/")
 def search_cafe():
     token = request.headers.get("token")
-    check_token_result = db.session.execute(db.select(Token).where(Token.token == token))
+    check_token_result = db.session.execute(
+        db.select(Token).where(Token.token == token)
+    )
     check_token = check_token_result.scalars().first()
     query = f"%{request.args.get('location')}%"
     if check_token:
@@ -221,14 +276,18 @@ def search_cafe():
         if all_chargers:
             return jsonify(chargers=[charger.to_dict() for charger in all_chargers])
         else:
-            return jsonify(error={"Not Found": "Sorry, we don't find a charger at that location."}), 404
+            return jsonify(
+                error={"Not Found": "Sorry, we don't find a charger at that location."}
+            ), 404
     return jsonify(error={"error": "You need a valid token."}), 401
 
 
-@app.route("/update/<charger_id>", methods = ["PATCH"])
+@app.route("/update/<charger_id>", methods=["PATCH"])
 def update(charger_id):
     token = request.headers.get("token")
-    check_token_result = db.session.execute(db.select(Token).where(Token.token == token))
+    check_token_result = db.session.execute(
+        db.select(Token).where(Token.token == token)
+    )
     check_token = check_token_result.scalars().first()
     new_description = request.form.get("description")
     result = db.get_or_404(Charger, charger_id)
@@ -236,12 +295,19 @@ def update(charger_id):
         if result:
             result.description = f"{new_description}"
             db.session.commit()
-            return jsonify(response={"success": "Successfully updated the description."}), 200
+            return jsonify(
+                response={"success": "Successfully updated the description."}
+            ), 200
         else:
-            return jsonify(error={"Not Found": "Sorry, a charger with that ID was not found in the database."}), 404
+            return jsonify(
+                error={
+                    "Not Found": "Sorry, a charger with that ID was not found in the database."
+                }
+            ), 404
     return jsonify(error={"error": "You need a valid token."}), 401
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
     # csv_to_db()
+
